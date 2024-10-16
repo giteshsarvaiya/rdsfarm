edd2ab6ff8e083343e4798cb2da538984335d2e4
Gitesh <gitesh.sarvaiya28@gmail.com>
2024-10-16 16:21:48 +0530

    og image edited

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
     siteName: "RD's Farm",
     images: [
       {
-        url: "/rdsfarm-og.png",
+        url: "https://www.rdsfarm.com/rdsfarm-og.png",
         width: 1200,
         height: 630,
         alt: "RD's Farm lush landscape",
